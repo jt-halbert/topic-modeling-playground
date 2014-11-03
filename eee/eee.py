f5c2e223ef04c425a30dcc6859a1a22fdf33f381
Mariah Wilcher <mwilcher@localhost.(none)>
2014-11-03 15:37:18 -0500

    Added writing allwords out and included more punctuation filtering

--- eee/eee.py
+++ eee/eee.py
@@ -11,12 +11,15 @@ from collections import Counter
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 stops = set(stopwords.words('english'))
 file = open("/home/mwilcher/enron.json", "r")
-corpusFile = open("/home/mwilcher/eee/outfile.txt","w")
+corpusFile = open("/home/mwilcher/eee/AllWords.txt","w")
+BOW_File = open("/home/mwilcher/eee/bagOfWords.txt","w")
 
-punct='`\'\-,()\.;<>:?&%$!^='
+
+punct='`\'\-,()\.;<>:?&%$!^=@{}#*[]'
 
 docOpen=0
 docFailed=0
+corpusTermsAll={}
 
 for line in file:
 	try:
@@ -28,17 +31,28 @@ for line in file:
 		message_id=email['headers']['Message-ID']
 		Subject=email['headers']['Subject']
 		docWords=[]
+		
+		#payload to tokens
 		tokens = [word for sent in sent_tokenize(payload) for word in word_tokenize(sent)]
 		docWords= filter(lambda word: word not in punct, tokens)
 		docWordsLower = [x.lower() for x in docWords ]
 		docWordsFilters = filter(lambda word: word not in stops,docWordsLower)
+
 		c = Counter(docWordsFilters)
-		for word in docWordsFilters:
-			corpusFile.write('%s\t%s\t%d\n' % (message_id,word, c[word]))	
+		for word in set(docWordsFilters):
+			BOW_File.write('%s\t%s\t%d\n' % (message_id,word, c[word]))
+			if word in corpusTermsAll:
+				corpusTermsAll[word]=corpusTermsAll[word] + c[word]
+			else:
+				corpusTermsAll[word]=c[word]
 			
 	except ValueError:
 		print 'Decoding JSON has failed'
 		docFailed+=1
 
 
+BOW_File.close()
+for word in corpusTermsAll:
+	corpusFile.write('%s\t%d\n' % (word,corpusTermsAll[word]))
+		
 	
